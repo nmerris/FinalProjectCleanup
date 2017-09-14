4e41738165ca96340fca6a084c670fe4abe6a256
student <behabtuhiwot@gmail.com>
2017-09-14 17:11:15 -0400

    add routes for teacherController and related html pages for that

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -74,4 +74,5 @@ public class AdminController
 		return "allcourses";
 	}
 
+
 }
--- src/main/java/com/example/demo/repositories/CourseRepo.java
+++ src/main/java/com/example/demo/repositories/CourseRepo.java
@@ -1,7 +1,11 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.Course;
+import com.example.demo.models.Person;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface CourseRepo extends CrudRepository<Course, Long> {
+    Set<Course> findByPersons(Person persons);
 }
