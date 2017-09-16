e682bc28f72a4cc2dd6d84816752c40a017f1801
nmerris <nate.merris@gmail.com>
2017-09-16 09:37:34 -0400

    add DataLoader class as a service, this will load roles for STUDENT, ADMIN, and TEACHER into database when program runs.  it checks to see if there are any existing roles, if there are, it does nothing.  note that no persons are being created anymore, you can just sign up for new ones and use your own for testing.  !!!NOTE: i recommend dropping your finaldb database, then setting your properties file to 'create' so that the entire databse is recreated, that way everything will be up to date and we will all be using the exact same database configuration, don't forget to set your properties file back to 'update' after the first run

---
+++ src/main/java/com/example/demo/services/DataLoader.java
@@ -0,0 +1,37 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Authority;
+import com.example.demo.repositories.AuthorityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    
+    @Autowired
+    AuthorityRepo authorityRepo;
+    
+    @Override
+    public void run(String... strings) throws Exception {
+        
+        // create three authorities and save them to the authority table
+        // first make sure there are no authorities already in the table so as not to cause a unique constraint violation
+        if( authorityRepo.count() == 0) {
+            Authority adminAuthority = new Authority();
+            adminAuthority.setRole("ADMIN");
+            authorityRepo.save(adminAuthority);
+
+            Authority teacherAuthority = new Authority();
+            teacherAuthority.setRole("TEACHER");
+            authorityRepo.save(teacherAuthority);
+
+            Authority studentAuthority = new Authority();
+            studentAuthority.setRole("STUDENT");
+            authorityRepo.save(studentAuthority);
+        }
+
+        // could also seed database with other data here
+    }
+}
