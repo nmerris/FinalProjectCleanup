dbc263b9d42c7732468982154ddf436fec95cc8b
yling0 <xyun2016@gmail.com>
2017-09-18 14:26:08 -0400

    change default account type to teacher

--- src/main/java/com/example/demo/repositories/AuthorityRepo.java
+++ src/main/java/com/example/demo/repositories/AuthorityRepo.java
@@ -3,11 +3,14 @@ package com.example.demo.repositories;
 import com.example.demo.models.Authority;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public interface AuthorityRepo extends CrudRepository<Authority, Long> {
     Authority findByRole(String role);
 
     Set<Authority> findByRoleIsOrRoleIs(String role1, String role2);
+
+    ArrayList<Authority>findByRoleIsOrRoleIsOrderByRoleDesc(String role1, String role2);
 
 }
